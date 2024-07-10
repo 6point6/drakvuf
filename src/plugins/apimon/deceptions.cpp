826056566b98a4abf80a750f75d43ead19f627a9
moyrum <moyrum.akhtar@6point6.c.uk>
2024-07-10 10:37:41 +0000

    recomit

--- src/plugins/apimon/deceptions.cpp
+++ src/plugins/apimon/deceptions.cpp
@@ -87,11 +87,58 @@ void deception_nt_create_file(drakvuf_t drakvuf, vmi_instance_t vmi, drakvuf_tra
         }
     }
     drakvuf_resume(drakvuf);
+    
 }
-void deception_lookup_account_sid_w(vmi_instance_t vmi, drakvuf_trap_info *info)
-{
-    vmi_pid_t curr_pid = info->attached_proc_data.pid;                        // Get PID of process
-    addr_t pSID = info->regs->rdx;                                            // Get address of PSID
+
+void deception_net_user_get_info(vmi_instance_t vmi, drakvuf_trap_info* info) {
+    ApimonReturnHookData* data = (ApimonReturnHookData*)info->trap->data; // Get the data from the trap
+    std::vector<uint64_t> temp_args = data->arguments; // Store all the arguments passed by the function
+    std::cout << "    *bufptr: 0x" << std::hex << temp_args[3] << "\n"; // Print the address of the 4th arg
+    vmi_pid_t curr_pid = data->target->pid;
+    addr_t bufptr = temp_args[3]; // Store address of bufptr
+    uint64_t pUserInfo_3 = 0; // Store address of User_Info_3 struct
+    uint64_t pUsri3_name = 0; // Store address of Usri3_name struct
+    
+    if (VMI_FAILURE == vmi_read_64_va(vmi, bufptr, curr_pid, &pUserInfo_3)) // Read address at pointer (arg3)
+    {
+        std::cout << "Error occured 1" << "\n";
+    }
+
+    std::cout << "pUserInfo_3: 0x" << std::hex << pUserInfo_3 << "\n";
+    if (VMI_FAILURE == vmi_read_64_va(vmi, (addr_t)pUserInfo_3, curr_pid, &pUsri3_name)) // Print address of pUserInfo_3
+    {
+        std::cout << "Error occured 2" << "\n";
+    }
+    
+    std::cout << "pUsri3_name: 0x" << std::hex << pUsri3_name << "\n"; // Print address of pointer to usri3_name
+    if (temp_args[2] == 3)
+    {
+        std::cout << "Found: USER_INFO_3 struct!" << "\n";
+        /*  Replace Tester with Batman
+            Batman = 42 00 61 00 74 00 6d 00 61 00 6e 00 */
+        uint8_t fake_user[12] = {66, 0, 97, 0, 116, 0, 109, 0, 97, 0, 110, 0};
+        for (uint8_t byte : fake_user)
+        {
+            if (VMI_FAILURE == vmi_write_8_va(vmi, (addr_t)pUsri3_name, curr_pid, &byte))
+            {
+                std::cout << "Writing to mem failed!" << "\n";
+                break;
+            }
+            pUsri3_name++; // move address 1 byte
+        }
+
+        std::cout << "Replaced username with 'Batman' !" << "\n";
+
+    } else if (temp_args[2] == 2) {
+        std::cout << "Found: USER_INFO_2 struct!" << "\n";
+    } else {
+        std::cout << "Unsupported USER_INFO_X struct!" << "\n";
+    }
+}
+
+void deception_lookup_account_sid_w(vmi_instance_t vmi, drakvuf_trap_info* info) {
+    vmi_pid_t curr_pid = info->attached_proc_data.pid; // Get PID of process
+    addr_t pSID = info->regs->rdx; // Get address of PSID
     uint8_t fake_SID[16] = {1, 1, 0, 0, 0, 0, 0, 5, 18, 0, 0, 0, 0, 0, 0, 0}; // Replace current user SID with system
     /*  REAL System user SID
         01 01 00 00 00 00 00 05 18 00 00 00 00 00 00 00 */
